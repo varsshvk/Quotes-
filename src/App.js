5bda0b7dddc3618066abae043bb40edbe2fa4041
varsshvk <108730017+varsshvk@users.noreply.github.com>
2023-04-25 11:56:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import { Table } from '@mui/material';
+
+import  Twitter  from './Twitter';
+function App() {
+
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  // http://api.quotable.io/random
+
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+  },[]);
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+          
+        }
+      )
+  }
+  return (
+    <div>
+   <h1>RANDOM QUOTE GENERATOR</h1> 
+    <div className="App">
+         <div id='quotestyle' className="quote">
+            <h2>{quote}</h2>
+
+            <small>-{author}-</small>
+         </div><br />
+         <button className="btn" onClick={fetchNewQuote}>Generate New Quote</button>
+    <Twitter/>
+         </div>
+    <h2>VARSSHA K[727721EUEE101]</h2>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Twitter.js
@@ -0,0 +1,11 @@
+
+
+export default function Twitter(){
+    return(
+        <div> <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D">
+    <img src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-twitter-1.png" alt ="twitter logo" id="logo"></img>
+    </a>
+    </div>
+    )
+}
+
